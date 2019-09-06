1c1d8bd2a4e2a39af88d0fa7f109165c67324790
hanxiao32 <hanxiao32@wanda.cn>
2019-09-06 11:08:07 +0800

    add

---
+++ HxControl/HxControl/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HxControl
+//
+//  Created by han xiao on 2019/9/6.
+//  Copyright © 2019 hanxiao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
